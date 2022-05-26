7c60ce824a2f8f0ada450ef2d03aa4269a1224b3
unknown <brunobn13@hotmail.com>
2022-05-25 22:46:45 -0300

    feat: Implementado cache para memorização de valores anteriormente

--- src/controllers/cep/cep.controllers.ts
+++ src/controllers/cep/cep.controllers.ts
@@ -7,6 +7,10 @@ import axios from 'axios';
 import GetCepDto from "../../dto/cep/getCep.dto";
 import Cep from "../../models/cep";
 
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
+const TIME_CACHE_EXPIRED = 5 * 60000;
+
 export class CepController {
 
     static postCep = async (req: Request, res: Response) => {
@@ -17,13 +21,21 @@ export class CepController {
             return res.send(validation);
 
         try {
+            const query = req.query.cep;
+
+            if (cache.has(query)) {
+                return res.send({ ...cache.get(query), cache: true })
+            }
+
             const { data } = await axios.get<Cep>
                 (`https://viacep.com.br/ws/${cep['cep']}/json/`);
 
             if (!GetCepDto.isCep(data))
                 throw "CEP inexistente ou inválido";
 
-            res.send(data);
+            cache.set(query, data, TIME_CACHE_EXPIRED);
+
+            res.send({ ...data, cache: false });
         } catch (exception) {
 
             return res.status(400).send(exception)
