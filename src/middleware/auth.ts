29186f19d31818f513a7b3875db1a5f3ebf22072
unknown <brunobn13@hotmail.com>
2022-05-27 07:49:42 -0300

    feat: Adicionado logica ao middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 export const authenticator = (req: Request, res: Response, next: NextFunction) => {
-    // TODO: verificar JWT
-    next();
+    const token = req.header("teste");
+
+    if (token === '123456') next();
+    else res.status(401).send({ code: 401, message: 'Unauthorized' })
 }
